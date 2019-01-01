3d8ede22e9b9763cd22c1d56d337228d36bd6044
chemoune <chemounetorres@yahoo.fr>
2019-01-01 02:36:24 +0100

    job spark creant des png

--- spark_maven/src/main/java/bigdata/TPSpark.java
+++ spark_maven/src/main/java/bigdata/TPSpark.java
@@ -1,48 +1,37 @@
 package bigdata;
 
-import java.util.List;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.RandomAccessFile;
-import java.util.ArrayList;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.hadoop.conf.Configuration;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
-import java.io.File;
-import java.io.IOException;
-import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
-
 public class TPSpark {
-	public static void main(String[] args) throws IOException {
-		
-		//SparkConf conf = new SparkConf().setAppName("TP Spark");
-		//JavaSparkContext context = new JavaSparkContext(conf);
-		
-		/*if (args.length == 0) {
-            System.out.println("No files provided.");
-            System.exit(0);
-        }*/
-		
-		//JavaRDD<String> textFile = context.textFile(args[0]);
-		File f = new File(/*args[0]*/"/home/chemoune/Bureau/bigdata/projet/N43W002.hgt");
-		FileInputStream fileInputStream = new FileInputStream(f);
+	
+	public static int[][] hgt2mat(FileStatus hgtFile, int lengMat) throws IOException {
+		InputStream inputStream = new FileInputStream("/home/chemoune/Bureau/projet_PLE/intro/"+hgtFile.getPath().getName());
 		
 		int srtm_ver = 1201;
 	    int height[][] = new int[1201][1201];
-	    System.out.println("P2\n"+srtm_ver+" "+srtm_ver);
 	    
-	    if(f.exists() && !f.isDirectory())
+	    if(!hgtFile.isDirectory())
 	    {
 	    	byte[] buffer = new byte[2]; 
-	        String filen = new String(f.toString());
+	        String filen = new String(hgtFile.getPath().toString());
 	        
 	        filen = filen.substring(filen.length()-11);
 	        //System.out.println(filen.substring(1, 3));
@@ -55,20 +44,20 @@ public class TPSpark {
 		    if (filen.indexOf(0)=='S' || filen.indexOf(0)=='s') lat *= -1;
 	        if (filen.indexOf(3)=='W' || filen.indexOf(3)=='w') lng *= -1;
 	        
-	        int buff0 = 0;
-	        int buff1 = 0;
+	        int unsigned0 = 0;
+	        int unsigned1 = 0;
 	        
 	        for (int i = 0; i<srtm_ver; ++i){
 	            for (int j = 0; j < srtm_ver; ++j) {
 	            	
-	            	if(fileInputStream.read(buffer) < 0) {
+	            	if(inputStream.read(buffer) < 0) {
 	                    System.out.println("Error reading file!");
 	                    System.exit(-1);
 	                }
 	            	
-	            	buff0 = ((int)buffer[0] < 0) ? (int)buffer[0] + 256 : (int)buffer[0];
-	            	buff1 = ((int)buffer[1] < 0) ? (int)buffer[1] + 256 : (int)buffer[1];
-	        	    height[i][j] = (buff0 << 8) | buff1;
+	            	unsigned0 = ((int)buffer[0] < 0) ? (int)buffer[0] + 256 : (int)buffer[0];
+	            	unsigned1 = ((int)buffer[1] < 0) ? (int)buffer[1] + 256 : (int)buffer[1];
+	        	    height[i][j] = (unsigned0 << 8) | unsigned1;
 	        	    
 	        	    if (height[i][j] > maxh)// || height[i][j] < 0)
 	        		    height[i][j] = maxh;
@@ -76,64 +65,56 @@ public class TPSpark {
 	        	    maxh = Math.max(maxh, height[i][j]);
 	            }
 	        }
-	    	
-	        maxh -= minh;
-	        //System.out.println("maxh = "+maxh);
-	        //System.out.println("minh = "+minh);
-	        
-	        //create buffered image object img
-	        BufferedImage img = new BufferedImage(srtm_ver, srtm_ver, BufferedImage.TYPE_INT_ARGB);
-	        //file object
-	        File ff = null;
-	        
-	        for (int j = 0; j<srtm_ver; ++j){
-	            for (int i = 0; i < srtm_ver; ++i) {
-		           int val = height[j][i] - minh;
-		           //System.out.print(val+" ");
-		           
-		            int a = val;//(int)(Math.random()*256); //alpha
-		            int r = val;//(int)(Math.random()*256); //red
-		            int g = val;//(int)(Math.random()*256); //green
-		            int b = val;//(int)(Math.random()*256); //blue
-		    
-		            int p = (a<<24) | (r<<16) | (g<<8) | b; //pixel
-		    
-		            img.setRGB(i, j, p);
-	            }
-	            System.out.println("\n");
-	        }
-	        //write image
-	        try{
-	          ff = new File("/home/chemoune/Bureau/Output.png");
-	          ImageIO.write(img, "png", ff);
-	        }catch(IOException e){
-	          System.out.println("Error: " + e);
-	        }
-	    	
 	    }else {
 	    	System.out.println("Empty file !");
+	    	System.exit(-1);
 	    }
+		return height;
+	}
+	
+	public static void createPng(int[][] matrice, String path) throws IOException {
+		BufferedImage img = new BufferedImage(matrice.length, matrice.length, BufferedImage.TYPE_INT_ARGB);
+        
+        File ff = null;
+        
+        for (int j = 0; j<matrice.length; ++j){
+            for (int i = 0; i < matrice.length; ++i) {
+	           int val = matrice[j][i];
+	           
+	            int a = val;//(int)(Math.random()*256); //alpha
+	            int r = val;//(int)(Math.random()*256); //red
+	            int g = val;//(int)(Math.random()*256); //green
+	            int b = val;//(int)(Math.random()*256); //blue
 	    
+	            int p = (a<<24) | (r<<16) | (g<<8) | b; //pixel
 	    
-	    /*
-	    int max = 0;
-	    for (int i = 0; i<srtm_ver; ++i)
-	        for (int j = 0; j < srtm_ver; ++j) {
-	            if (height[i][j] < 30000)
-	            	max = Math.max(height[i][j], max);
-	        }
-	    System.out.println("P2");
-	    System.out.println(srtm_ver+" "+srtm_ver);
-	    System.out.println(max);
-	    for (int i = 0; i<srtm_ver; ++i) {
-	        for (int j = 0; j < srtm_ver; ++j) {
-	            System.out.print(height[i][j]+" ");
-	        }
-	        System.out.print("");
-	    }
-	    */
-	    
-	    
-	    System.out.println("Succefully.");
+	            img.setRGB(i, j, p);
+            }
+        }
+        
+        try{
+          ff = new File(path.toString());
+          ImageIO.write(img, "png", ff);
+        }catch(IOException e){
+          System.out.println("Error: " + e);
+        }
+	}
+	
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		
+		SparkConf conf = new SparkConf().setAppName("TP Spark").setMaster("local[2]");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		
+		//FileSystem hdfs = FileSystem.get(new URI("hdfs://localhost:9000"), conf);
+		//Path file = new Path("hdfs://localhost:9000/achemoune/file.txt");
+		
+		RemoteIterator<LocatedFileStatus> fileStatusListIterator = FileSystem.get(sc.hadoopConfiguration()).listFiles(new Path("/home/chemoune/Bureau/projet_PLE/intro"), true);
+		
+		while(fileStatusListIterator.hasNext()) {
+			LocatedFileStatus fileEntry = fileStatusListIterator.next();
+			if(!fileEntry.isDirectory() && fileEntry.getPath().getName().endsWith(".hgt")) {
+				createPng(hgt2mat(fileEntry, 1201), "/home/chemoune/Bureau/"+fileEntry.getPath().getName().substring(0, fileEntry.getPath().getName().indexOf(".")+1)+"png");
+			}
+		}
 	}
 }
--- web_app/server.js
+++ web_app/server.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+var WebHDFS = require('webhdfs');
+
+var hdfs = WebHDFS.createClient(/*{user: 'webuser', host: 'localhost', port: 9000, path: 'hdfs://localhost:9000'}*/);
+
+var remoteFileStream = hdfs.createReadStream('hdfs://localhost:9000/data/Output.png');
+
 app.get('/', function(req, res) {
     res.sendfile('./index.html');
 });
@@ -11,5 +17,5 @@ app.use(function(req, res, next){
 });
 
 app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
+  console.log('PLE app listening on port 3000!');
 });
