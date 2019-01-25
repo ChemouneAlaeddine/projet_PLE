32f94271321ab71ddfcfb7895eff14032b956eb7
Alaeddine Chemoune <achemoune@emi.u-bordeaux1.fr>
2019-01-25 17:51:39 +0100

    web api

--- PLE_server/app.js
+++ PLE_server/app.js
@@ -1,16 +1,36 @@
 var express = require("express");
 var path = require("path");
 var app = express();
+const hbase = require('hbase');
+//const HBase = require('hbase-client');
+const HBase = require('hbase-rpc-client');
+
+console.log("something");
+
+//var client = hbase({ host: 'young', port: 16000 });
+client = HBase({
+  zookeeperHosts: ['young'],
+  zookeeperRoot: ['/hbase']
+})
+
 app.use( express.static( "./ejs" ) );
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
     res.render(path.resolve('ejs/index.ejs'));
-  });
-  app.get('/page', function (req, res) {
-    res.render(path.resolve('ejs/page.ejs'));
-  });
+});
 
-app.listen(8080);
+app.get('/page', function (req, res) {
+  console.log("getting");
+  const data = client.getScanner('achemoune');
+  var s = data.toArray((err, res) => {
+    console.log(res[0].columns[0].value);
+    let json = JSON.stringify(res[0].columns[0].value);
+    console.log(json);
+  });
+  res.render(path.resolve('ejs/page.ejs'));
+});
 
-console.log("Running at Port 8080");
+app.listen(4371);
+
+console.log("Running at Port 4371");
